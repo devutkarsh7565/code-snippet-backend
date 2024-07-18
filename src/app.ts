4f74081ae5f3c4b0b8f13c49325fb654c342e3ef
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2024-07-18 17:03:46 +0530

    add middleware for cors and json response in app.ts file

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,16 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 //these are the middleware methods,if you use the sintax app.use then you are using middleware
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({
+  path: "./.env",
+});
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
