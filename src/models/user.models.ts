833070e3c4c1fd54ae296b73ecebd9e8967c4709
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2024-07-18 18:03:49 +0530

    add an mongoose custom method for checking the password

--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -60,3 +60,9 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+//mongoose custom methods
+
+userSchema.methods.isPasswordCorrect = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
