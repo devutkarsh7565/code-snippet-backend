15eb0f75a91c4b3d7e0e9f37e44073a193061ae0
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2024-07-18 21:06:45 +0530

    add register user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,50 +1,78 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import { User } from "../models/user.model";
+import { asyncHandler } from "../utils/asyncHandler";
+import { uploadOnCloudinary } from "../utils/cloudinary";
+import { IUser } from "../types/user.type";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  const { name, email, password, avatar } = req.body;
+const createUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, password } = req.body;
 
-  if (!name || !email || !password || !avatar) {
-    const error = createHttpError(400, "All fields are required");
-    return next(error);
-  }
-
-  // database call
-
-  try {
-    const user = await User.findOne({ email });
-
-    if (user) {
-      const error = createHttpError(400, "user already exist with this email");
+    if (!name || !email || !password) {
+      const error = createHttpError(400, "All fields are required");
       return next(error);
     }
-  } catch (err) {
-    return next(createHttpError(500, "Error while getting user"));
-  }
 
-  const avatarLoca;
+    // database call
 
-  let newUser: User;
+    try {
+      const user = await User.findOne({ email });
 
-  try {
-    newUser = await userModel.create({
-      name,
-      email,
-      password: hashedPassword,
+      if (user) {
+        const error = createHttpError(
+          400,
+          "user already exist with this email"
+        );
+        return next(error);
+      }
+    } catch (err) {
+      return next(createHttpError(500, "Error while getting user"));
+    }
+
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+    const avatarLocalPath = files?.avatar?.[0].path;
+
+    if (!avatarLocalPath) {
+      const error = createHttpError(400, "avatar is required");
+      return next(error);
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatar) {
+      const error = createHttpError(500, "Error while uploading avatar");
+      return next(error);
+    }
+
+    let newUser: IUser;
+
+    try {
+      newUser = await User.create({
+        name,
+        email,
+        password,
+        avatar,
+      });
+    } catch (err) {
+      return next(createHttpError(500, "Error while creating user"));
+    }
+
+    const createdUser = await User.findById(newUser._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!createdUser) {
+      const error = createHttpError(500, "Error while creating user");
+      return next(error);
+    }
+
+    return res.status(201).json({
+      success: true,
+      message: "user created successfully",
+      createdUser,
     });
-  } catch (err) {
-    return next(createHttpError(500, "Error while creating user"));
   }
+);
 
-  //Token Generation JWT
-  try {
-    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-      expiresIn: "7d",
-    });
-
-    res.status(201).json({ id: newUser._id, accessToken: token });
-  } catch (err) {
-    return next(createHttpError(500, "Error while signing jwt token"));
-  }
-};
+export { createUser };
